1187d81c36fa6c65214f03f261b285e847cf65d9
Evgenii Korobkov <evgeny.korobkov@teampicnic.com>
2026-05-18 20:09:36 +0200

    feat: improve aliasing

--- gogol_cli.sh
+++ gogol_cli.sh
@@ -3,7 +3,13 @@
 #   export GOGOL_CLI_DIR="/path/to/gogol-cli"
 #   source "$GOGOL_CLI_DIR/gogol_cli.sh"
 
-gogol() {
-    uv run --project "$GOGOL_CLI_DIR" --env-file "$GOGOL_CLI_DIR/.env" python -m gogol_cli "$@"
+alias gogol='noglob _gogol_run'
+
+_gogol_run() {
+    local args=()
+    for arg in "$@"; do
+        args+=("${arg%%\?*}")
+    done
+    uv run --project "$GOGOL_CLI_DIR" --env-file "$GOGOL_CLI_DIR/.env" python -m gogol_cli "${args[@]}"
 }
 
